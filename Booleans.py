6b9175e3d5ee98b1a236bc6ad20d1da9743d9d6c
runninrebel <linares.jc@protonmail.com>
2024-03-09 14:41:02 -0800

    added boolean logic

---
+++ Booleans.py
@@ -0,0 +1,60 @@
+name = "Kels"
+age = 34
+
+if name == "Kels" and age == 34:
+    print("Your name is %s, and your are is %d" % (name, age))
+
+
+if name == "Rick" or name == "Kels":
+    print("Hello")
+    
+if name in ["Chef", "Kels"]:
+    print("Your name is either Chef or Kels")
+    
+
+statement = False
+another_statement = True
+if statement is True:
+    pass
+elif another_statement is True:
+    pass
+else:
+    pass
+
+#the "is" operator does not match the values of the variables,
+#but the instance themselves
+x = [1,2,3]
+y = [1,2,3]
+print(x == y)
+print(x is y)
+
+t = "T"
+r = "T"
+print(t is r)
+
+#use "not" instead of "!"
+print(not False)
+print(not False) == (False)
+
+number = 18
+second_number = 0
+first_array = [1,2,3]
+second_array = [1,2]
+
+if number > 15:
+    print("6")
+
+if first_array:
+    print("5")
+    
+if len(second_array) == 2:
+    print("4")
+    
+if len(first_array) + len(second_array) == 5:
+    print("3")
+
+if first_array and second_array[0] == 1:
+    print("2")
+    
+if not second_number:
+    print("1")
--- Strings.py
+++ Strings.py
@@ -32,3 +32,36 @@ data = ("Kels", "Sells", 53.55)
 format_string = "Hello %s %s. Your current balance is $%f"
 
 print(format_string % data)
+
+
+astring = "Hello World!"
+print(len(astring)) # 12
+print(astring.index("o")) #4
+print(astring.count("l")) # 3
+print(astring[3:7]) #lo w  
+print(astring[3:7:2]) #l
+print(astring[-3]) #3rd char from end = l
+print(astring[::-1]) #reverse a string 
+print(astring.upper())
+print(astring.lower())
+print(astring.startswith("Hell"))
+print(astring.endswith("lol"))
+print(astring.split(" "));
+
+s = "Strings are Awesome!"
+print("Length of s = %d" % len(s))
+print("The first occurence of the letter a = %d" % s.index("a"))
+print("a occurs %d" % s.count("a"))
+print("The first five characters are '%s'" % s[:5])
+print("The next five charaters are '%s'" % s[5:10])
+print("The thirteenth character is '%s'" % s[12])
+print("The charaters with odd index are '%s'" % s[1::2])
+print("The last five charaters are '%s'" % s[-5:])
+
+if s.startswith("Str"):
+    print("String starts with 'Str'. Great!")
+
+if s.endswith("ome!"):
+    print("String ends with 'ome!. Great!")
+
+print("Split the words of the string: %s" % s.split(" "))
